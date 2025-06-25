cac38e4d720c221bf1a6c4532ff959fde23c4ae5
LYJ <343577362@qq.com>
2025-06-25 10:07:58 +0800

    fix build script path issue

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-# 安装依赖，禁用build isolation
+# 切换到项目根目录
+cd ../
+
+# 升级 pip
 pip install --upgrade pip
+
+# 安装依赖
 pip install --no-build-isolation -r requirements.txt
 
-# 收集静态文件（如果你有）
+# 收集静态文件
 python manage.py collectstatic --noinput
