976eb8a11ae6622ab1e8c9a66eecd41a969dee53
LYJ <343577362@qq.com>
2025-06-25 10:16:11 +0800

    remove root build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# 安装依赖，禁用build isolation
-pip install --upgrade pip
-pip install --no-build-isolation -r requirements.txt
-
-# 收集静态文件（如果你有）
-python manage.py collectstatic --noinput
